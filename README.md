85e0888d1ca4b99a85f64021c6979e957c8f552a
Tymko Tecca <tymko.tecca@gmail.com>
2020-02-19 00:45:21 -0500

    Actually do stuff

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # eslint-plugin-forbidden-package
-DO NOT IMPORT THAT WHICH IS... _FORBIDDEN_
+DO NOT IMPORT THAT WHICH IS... *FORBIDDEN*.
 
-An eslint plugin (really, just a rule) for explicitly yelling when someone imports blacklisted libraries/packages directly.
+An eslint plugin (really, just a rule) for explicitly yelling when someone imports blacklisted libraries/packages directly. Now when people import `xDate` directly, instead of a wrapped/abstracted library, you can yell at 'em. This is hacky. It's set up to not do anything without settings.
+
+Usage like so: *(pretend you're in an `.eslintrc`)*
+
+```javascript
+{
+	"plugins": ["forbidden-package"],
+	"rules": {
+		"forbidden-package/no-forbidden-package":["error", ["moment", "immutable", "xDate", "styled-components"]]
+	}
+}
+```
+
+On the future todo list -- get this ready to warn/error separately.
--- rules/no-forbidden-package.js
+++ rules/no-forbidden-package.js
@@ -1,17 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const getOptionsValid = function (options) {
+    if (!options || !options.length || !Array.isArray(options[0])) return false;
+    return true;
+}
 
 const create = function (context) {
+    if (!getOptionsValid(context.options)) return {};
     return {
         ImportDeclaration(node) {
+            const blacklist = context.options[0];
             if (!node || !node.source || !node.source.value) return;
             const importPath = node.source.value;
-
-            if (isModule) {
+            if (blacklist.indexOf(importPath) !== -1) {
                 context.report({
                     node,
-                    message: "Do not import another module's internal files or functionality.",
-                });
+                    message: `It is FORBIDDEN to import ${importPath} directly.`,
+                    suggest: [{
+                        desc: "Remove the FORBIDDEN import.",
+                        fix: function (fixer) {
+                            fixer.remove(node);
+                        }
+                    }]
+                })
             }
         }
     };
